900afcff52fc861b33d8fa64eba356b740556fb0
stanislav <nikitaprokopenkolox@gmail.com>
2023-12-03 18:52:30 +0200

    ...

---
+++ js.js
@@ -0,0 +1,55 @@
+let wrapper = document.querySelector('.grid')
+newResponse(`https://my-json-server.typicode.com/stasik23/test2/products`)
+
+function newResponse(URL,){
+    let xhr = new XMLHttpRequest();
+    const url = URL
+    xhr.open('GET', url)
+    xhr.responseType = 'json'
+    xhr.send();
+    xhr.onload = function(){
+        state = xhr.response
+        console.log(xhr.response);
+        drawList(state)
+    }
+}
+
+function seller() {
+
+    
+}
+
+function getProductName(name){
+    let xhr = new XMLHttpRequest();
+    const url = `https://my-json-server.typicode.com/stasik23/test2/products/${name}`
+    xhr.open('GET', url)
+    xhr.responseType = 'json'
+    xhr.send();
+    if(xhr.status != 200){
+        console.log('Чепуха галимая');
+    }else{
+        console.log(xhr.response)
+    }
+    xhr.onload = function(){
+        console.log(xhr.response.name);
+        console.log(xhr.response.price);
+        console.log(xhr.response.id);
+        getPokemonIp(xhr.response);
+      
+    }
+}
+
+function drawList(product){
+    pokeWrapper.innerHTML = `
+    <div class="card">
+        <div class="title">${product.name}</div>
+        <hr>
+        <div class="name">${pokemon.name}</div>
+        <hr>
+        <img class="img" src="${pokemon.sprites.front_default}"/>
+        <hr>
+        <div class="abilities">${pokemon.abilities.map(e=>(e.ability.name))}
+        </div>
+    </div>
+    `
+}
